e10bceba4ef6667fd5435350e98a19ad4899bb14
glemi <cnyffeler@gmail.com>
2014-01-02 15:41:21 +0100

    Added screen dump routines for Configuration

--- CmiAppTest/src/ch/epfl/cmiapp/test/XmlLoadedEquipmentTest.java
+++ CmiAppTest/src/ch/epfl/cmiapp/test/XmlLoadedEquipmentTest.java
@@ -3,6 +3,7 @@ package ch.epfl.cmiapp.test;
 import java.io.*;
 
 import org.w3c.dom.*;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 import android.test.AndroidTestCase;
@@ -11,6 +12,10 @@ import javax.xml.parsers.*;
 
 import ch.epfl.cmiapp.R;
 import ch.epfl.cmiapp.core.*;
+import ch.epfl.cmiapp.core.XmlExtractor.ItemNotFoundException;
+import ch.epfl.cmiapp.core.Configuration.*;
+
+import static java.lang.System.*;
 
 import junit.framework.TestCase;
 
@@ -18,7 +23,7 @@ public class XmlLoadedEquipmentTest extends AndroidTestCase
 {
 	private static String xmlfile = "/home/glemi/git/android-cmiapp/CmiApp/res/raw/cmitools.xml";
 	
-	public void testXmlLoadedEquipment() throws ParserConfigurationException, SAXException, IOException
+	public void testXmlLoadedEquipment() throws ItemNotFoundException, ParserConfigurationException, SAXException, IOException
 	{
 		InputStream stream = super.getContext().getResources().openRawResource(R.raw.cmitools);
 		
@@ -42,7 +47,38 @@ public class XmlLoadedEquipmentTest extends AndroidTestCase
 			{
 				Equipment equipment = new XmlLoadedEquipment(xmlNode);
 				String key = equipment.getMachId();
+				displayEquipment(equipment);
 			}
+		}
+	}
+	
+	
+	public void displayEquipment(Equipment eq)
+	{
+		
+		out.println("::Equipment " + eq.getName() + " (" + eq.getMachId() + ") :::::::::::::::::::::::::::::::::::");
+		out.println("  Full String: " + eq.getFullString());
+		out.println("  Category: " + eq.getCategory());
+		out.println("  Zone: " + eq.getZoneString());
+		out.println("  Supplement: " + eq.getSupplement());
+		out.println("  Slot Length: " + eq.getSlotLength());
+		
+		if (eq.isConfigurable()) displayConfiguration(eq.getConfig());
+		
+	}
+	
+	public void displayConfiguration(Configuration cf)
+	{
+		out.println("  :Configuration");
+		
+		for (Setting s : cf)
+		{
+			out.println("   ::Setting " + s.getTitle());
+			out.println("     Cmi-Name: " + s.getName());
+			out.println("     id: " + s.getId());
+			out.println("     Value: " + s.getValue());
+			out.println("     Required: " + s.getRequired().toString());
+			out.println("     Displayed: " + s.getsDisplayed());
 		}
 	}
 	
