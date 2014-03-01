6f5d45da69d6ddf948ac31d821dd3d44e6b2c275
glemi <cnyffeler@gmail.com>
2014-03-01 11:05:08 +0100

    CmiAccount class
    Holds credentials and other user account data.

--- CmiApp/src/ch/epfl/cmiapp/util/CmiAccount.java
+++ CmiApp/src/ch/epfl/cmiapp/util/CmiAccount.java
@@ -4,20 +4,22 @@ import java.io.InputStream;
 
 public class CmiAccount
 {
-	
 	private String username;
 	private String password;
+	private String userid;
+	
+	private String fullName;
 	private String email;
 	
 	private String sciper;
-	private String GasparId;
-	private String GasparPassword;
+	private String gasparId;
+	private String gasparPassword;
 	
 	private static CmiAccount instance;
 	
-	public static void instance()
+	public static CmiAccount instance()
 	{
-		
+		return instance;
 	}	
 	
 	public class Builder
@@ -36,4 +38,20 @@ public class CmiAccount
 		return null;
 		
 	}
+	
+	public String getUsername() { return username; }
+	public String getPassword() { return password; }
+	public String getUserId()  { return userid; }
+	public String getEmail() { return email; } 
+	public String getSciper() { return sciper; } 
+	public String getGasparId() { return gasparId; }
+	public String getGasparPassword() { return gasparPassword; } 
+	
+	public void setUsername(String username) { this.username = username; }
+	public void setPassword(String password) { this.password = password; }
+	public void setUserid(String userid) { this.userid = userid; }
+	public void setEmail(String email) { this.email = email; }
+	public void setSciper(String sciper) { this.sciper = sciper; }
+	public void setGasparId(String gasparId) { gasparId = gasparId; } 
+	public void setGasparPassword(String gasparPassword) { gasparPassword = gasparPassword; }
 }
