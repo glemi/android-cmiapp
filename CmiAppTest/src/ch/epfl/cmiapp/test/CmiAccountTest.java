3928fb9d82e79144310bbf3eed462f68c9443873
glemi <cnyffeler@gmail.com>
2014-03-03 21:19:43 +0100

    Test Classes for CmiAccount etc.
    CmiAccountTest
    CmiServerConnectionTest
    CmiSshTunnelTest

--- CmiAppTest/src/ch/epfl/cmiapp/test/CmiAccountTest.java
+++ CmiAppTest/src/ch/epfl/cmiapp/test/CmiAccountTest.java
@@ -1,6 +1,17 @@
 package ch.epfl.cmiapp.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import com.jcraft.jsch.JSchException;
+
+import junit.framework.Assert;
 import ch.epfl.cmiapp.util.CmiAccount;
+import ch.epfl.cmiapp.util.CmiServerConnection;
 import android.test.AndroidTestCase;
 
 public class CmiAccountTest extends AndroidTestCase
@@ -13,12 +24,39 @@ public class CmiAccountTest extends AndroidTestCase
 		account = new CmiAccount();
 		
 		account.setGasparId("cnyffele");
+		account.setSciper("167382");
 		account.setGasparPassword("2branchGraphene");
 		account.setUserid("user1262");
 		account.setUsername("cnyffeler");
 		account.setPassword("clemens");
-		
+		account.setupSshTunnel();
 		
 		super.setUp();
 	}
+	
+	public void testSetupSshTunnel() throws JSchException
+	{
+		boolean ok = account.setupSshTunnel();
+		Assert.assertTrue(ok);		
+	}
+
+	public void testGetServerConnection() throws IOException
+	{
+		CmiServerConnection server = account.getServerConnection();
+		InputStream stream = server.getMainPage();
+		
+		Document document = Jsoup.parse(stream, null, null);
+		Element element = document.select("td[colspan=3]").first();
+		System.out.print(element.ownText());
+		
+		boolean ok = element.ownText().contains("Welcome");
+	}
+
+	public void testCloseSshTunnel() throws JSchException 
+	{
+		account.closeSshTunnel();
+		boolean closed = account.isSshTunnelConnected();
+		Assert.assertTrue(closed);
+	}
+	
 }
